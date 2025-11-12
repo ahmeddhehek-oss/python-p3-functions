e9c186f1589742ef08efac3098a0677e8ccbb9f4
Ahmed <ahmeddhehek@gmail.com>
2025-11-12 15:56:00 +0300

    python-functions

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
-def greet(name):
-    pass
+def greet(Ahmed):
+    print(f"Hello, {Ahmed}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+    return num1 + num2
 
 def halve(number):
-    pass
+    if isinstance(number, (int, float)):
+        return number / 2
+    return None  
+    
+
+
